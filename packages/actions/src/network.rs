76bf708dc5eccbf5a5c6c3c71463313163a66232
Paulo Martins <paulormart@users.noreply.github.com>
2025-12-10 12:28:21 +0000

    change reconnecting char

--- packages/actions/src/network.rs
+++ packages/actions/src/network.rs
@@ -13,7 +13,7 @@ impl std::fmt::Display for ConnectionState {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
             Self::Idle => write!(f, "-"),
-            Self::Connecting => write!(f, "⇜ / ⇝"),
+            Self::Connecting => write!(f, "↺"),
             Self::Connected(block_number) => write!(f, "#{}", block_number),
             Self::Error(_) => write!(f, "✗"),
         }
