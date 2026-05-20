f6929299c92fb1a9033b651dad9e66a5070107c9
Paulo Martins <paulormart@users.noreply.github.com>
2026-05-20 12:11:13 +0100

    remove unused extrinsics package

--- packages/extrinsics/src/lib.rs
+++
@@ -1,6 +0,0 @@
-pub mod balances;
-pub mod proxy;
-pub mod staking;
-mod submit;
-
-pub use submit::submit_as_proxy;
--- packages/extrinsics/src/staking.rs
+++
@@ -1,8 +0,0 @@
-use subxt::{
-    dynamic::{tx, Value},
-    tx::DynamicPayload,
-};
-
-pub fn chill() -> DynamicPayload {
-    tx("Staking", "chill", vec![] as Vec<Value>)
-}
